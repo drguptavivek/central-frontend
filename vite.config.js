958fe349ad6c45f12aca8c3a46bc8a2465834aa2
Vivek Gupta <drguptavivek@yahoo.com>
2025-12-17 17:53:41 +0530

    chore: enable central-dev proxy and version endpoint in dev

--- vite.config.js
+++ vite.config.js
@@ -32,6 +32,7 @@ const proxyPaths = [
 const devServer = {
   host: true,
   port: 8989,
+  allowedHosts: ['central-dev'],
   proxy: Object.fromEntries(proxyPaths.map(path => [path, 'http://localhost:8686'])),
   // Because we proxy to nginx, which itself proxies to Backend and other
   // things, the dev server doesn't need to allow CORS. CORS is already limited
